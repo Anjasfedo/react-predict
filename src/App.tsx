62bb20ad7be12487919be43a3d96f5c14be40e2d
AnjasFedo <fedoaf98@gmail.com>
2024-05-18 15:29:24 +0700

    Do// Page Base, Link, and Update Env Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import useAuthStore from "@stores/authStore";
 import { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import Login from "@src/components/Login";
 import Register from "@src/components/Register";
 import Dashboard from "@src/components/Dashboard";
@@ -11,6 +11,8 @@ function App() {
     (state) => state.initializeAuthListener
   );
 
+  const user = useAuthStore((state) => state.user);
+
   useEffect(() => {
     const unsubscribe = initialAuthListener();
     return () => {
@@ -19,16 +21,38 @@ function App() {
   }, [initialAuthListener]);
 
   return (
-    <>
+    <div className="flex min-h-screen flex-col items-center justify-between max-w-5xl mx-auto relative">
       <BrowserRouter>
+        <div className="flex">
+          <nav>
+            <ul className="flex space-x-2">
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </nav>
+          <p className="absolute right-0">User: {user ? user.email : "Not signed in"}</p>
+        </div>
         <Routes>
           <Route path="/login" Component={Login} />
           <Route path="/register" Component={Register} />
-          <Route path="/dashboard" Component={Dashboard} />
+          <Route
+            path="/dashboard"
+            element={user ? <Dashboard /> : <Navigate to="/login" />}
+          />
           <Route path="/" Component={Home} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
