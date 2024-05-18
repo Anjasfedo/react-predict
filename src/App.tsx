4b5b0eb7412f6ccac2a099f4895327ee137a968e
AnjasFedo <fedoaf98@gmail.com>
2024-05-18 13:40:38 +0700

    Do// The App Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import { Button } from "@chakra-ui/react";
+import { Switch } from "@chakra-ui/react";
+import useAuthStore from "@stores/authStore";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Navigate } from "react-router-dom";
+import Login from "@src/components/Login";
+import Register from "@src/components/Register";
+import Dashboard from "@src/components/Dashboard";
+import Home from "@src/components/Home";
 
 function App() {
+  const initialAuthListener = useAuthStore(
+    (state) => state.iniinitializeAuthListener
+  );
+
+  const user = useAuthStore((state) => state.user);
+
+  useEffect(() => {
+    initialAuthListener();
+    return () => {};
+  }, [initialAuthListener]);
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button colorScheme="blue">Button</Button>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+          <Route path="/dashboard">
+            {user ? <Dashboard /> : <Navigate to="/login" />}
+          </Route>
+          <Route path="/" Component={Home} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
