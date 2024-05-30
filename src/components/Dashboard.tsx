888dc2cbcd6e39460d871fd65968bc97361a4c63
AnjasFedo <fedoaf98@gmail.com>
2024-05-30 21:27:26 +0700

    Do// Post Data with React Form Hook

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,39 +1,89 @@
-import { fetchData } from "@libs/fetchApi";
-import { useQuery } from "react-query";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+} from "@chakra-ui/react";
+import { postData } from "@libs/fetchApi";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-const fetchDashboardData = async () => {
-  try {
-    const data = await fetchData("/v1/posts/");
+export interface FormData {
+  p1: string;
+  p2: string;
+  p3: string;
+  p4: string;
+  p5: string;
+  p6: string;
+  p7: string;
+  p8: string;
+  p9: string;
+  p10: string;
+}
 
-    return data;
-  } catch (error) {
-    throw new Error("Failed to fetch data");
-  }
-};
+const featureNames: (keyof FormData)[] = [
+  "p1",
+  "p2",
+  "p3",
+  "p4",
+  "p5",
+  "p6",
+  "p7",
+  "p8",
+  "p9",
+  "p10",
+];
 
 const Dashboard = () => {
-  const { data, isLoading, isError, error } = useQuery(
-    "dashboardData",
-    fetchDashboardData
-  );
+  const {
+    register,
+    handleSubmit,
+  } = useForm<FormData>();
+  const [predictedLevel, setPredictedLevel] = useState(null);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      const response = await postData({ ...data }, "/api/predict/");
+      setPredictedLevel(response.predicted_levels[0]);
+    } catch (error) {
+      console.error("Error making prediction:", error);
+    }
+  };
 
-  if (isError) {
-    return <div>Error: {error.message}</div>;
-  }
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {data.posts.map((post) => (
-        <>
-          <h1>{post.title}</h1>
-          <p>{post.content}</p>
-          <img src={post.imageURL} alt="" />
-        </>
-      ))}
+    <div className="App">
+      <h1>Stress Level Predictor</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={4}>
+          {featureNames.map((feature) => (
+            <FormControl key={feature}>
+              <FormLabel>{feature.toUpperCase()}</FormLabel>
+              <RadioGroup
+                name={feature}
+                defaultValue="1"
+                {...register(feature)}
+              >
+                <Stack direction="row">
+                  <Radio value="1">1</Radio>
+                  <Radio value="2">2</Radio>
+                  <Radio value="3">3</Radio>
+                  <Radio value="4">4</Radio>
+                </Stack>
+              </RadioGroup>
+            </FormControl>
+          ))}
+        </Stack>
+        <Button mt={4} colorScheme="teal" type="submit">
+          Predict
+        </Button>
+      </form>
+      {predictedLevel && (
+        <div>
+          <h2>Predicted Stress Level: {predictedLevel}</h2>
+        </div>
+      )}
     </div>
   );
 };
